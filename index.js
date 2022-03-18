4051b53faf39e339bc1aafe25c0096381124e17b
Maksim <familinov@gmail.com>
2022-03-18 21:32:04 +0500

    https

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const port = 3000
+const port = 3001
+const https = require('https')
+const fs = require('fs')
+const httpsOptions = {
+    key: fs.readFileSync("server.key"),
+    cert:fs.readFileSync("server.cert")
+}
 
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -9,4 +15,5 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'))
 })
 
+https.createServer(httpsOptions, app)
 app.listen(port)
